e73dc8e590d4a8c05497150839cc7a77a52dfe44
Emmanuel Mugogo <Emmanuel@Emmanuels-MacBook-Pro.local>
2017-03-10 11:06:48 -0500

    initial commit for a project with gitignore initials

---
+++ src/main/java/review/Review.java
@@ -0,0 +1,33 @@
+package review;
+
+public class Review {
+	
+	long id;
+	String title;
+	String imageUrl;
+	String reviewCategory;
+	String content;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	
+	public String getReviewCategory() {
+		return reviewCategory;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	
+
+}
