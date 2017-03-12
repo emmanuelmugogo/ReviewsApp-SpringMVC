a4e57c327a8567623042618cb455ec56b7b5c184
Emmanuel Mugogo <Emmanuel@Emmanuels-MacBook-Pro.local>
2017-03-11 22:47:17 -0500

    collections and views done

--- src/main/java/review/ReviewRepository.java
+++ src/main/java/review/ReviewRepository.java
@@ -17,13 +17,14 @@ public class ReviewRepository {
 	public ReviewRepository () {
  	    long id = 1l;
 		
-		Review review = new Review(1, "title", "imageU", "Categ", "cont");
+		Review review = new Review(1, "Chucky", "/images/chucky.jpg", "Toys", "Stuff'd animal!");
 		reviews.put(id, review);
 		
-		reviews.put(2l, new Review(2, "Iphone", "imageUrl", "mobiele Phones", "calls and Messages"));
-		reviews.put(3l, new Review(3, "Bread", "imageUrl", "Food", "Wheat"));
-		reviews.put(4l, new Review(4, "Beer", "imageUrl", "drinks", "alcohol and water"));
-		reviews.put(5l, new Review(5, "Mazda", "imageUrl", "vehicles", "Metals and Plastics"));
+		reviews.put(2l, new Review(2, "Iphone", "/images/phone.jpg", "mobiele Phones", "calls and Messages"));
+		reviews.put(3l, new Review(3, "Bread", "/images/bread.jpg", "Food", "Wheat"));
+		reviews.put(4l, new Review(4, "Beer", "/images/beer.jpg", "drinks", "alcohol and water"));
+		reviews.put(5l, new Review(5, "Mazda", "/images/mazda.jpg", "vehicles", "Metals and Plastics"));
+		reviews.put(6l, new Review(6, "Rolex", "/images/rolex.jpg", "Watches", "Expensive stuff just put together"));
 	}
 	
 	public Collection<Review> findAll() {
